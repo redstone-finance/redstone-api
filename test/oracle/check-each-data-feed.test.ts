630ec116bb4550c13e66185b391368691d937339
hatskier <suvorovalex1997@gmail.com>
2022-04-12 18:51:57 +0200

    updated regular tests based on github actions

---
+++ test/oracle/check-each-data-feed.test.ts
@@ -0,0 +1,19 @@
+import redstone from "../../src/index";
+import { DataFeedId, DataSourcesConfig } from "../../src/oracle/redstone-data-feed";
+
+const dataFeedIds = [
+  "redstone",
+  "redstone-stocks",
+  "redstone-rapid",
+  "redstone-avalanche",
+  "redstone-avalanche-prod",
+] as DataFeedId[];
+
+describe("Test all data feeds", () => {
+
+  for (const dataFeedId of dataFeedIds) {
+    test(`Checking data feed: ${dataFeedId}`, async () => {
+      await redstone.oracle.getFromDataFeed(dataFeedId);
+    });
+  }
+});
