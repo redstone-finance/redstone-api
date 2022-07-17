3670c89b02c3f97235c95392d49017f6cd8e7b82
hatskier <suvorovalex1997@gmail.com>
2022-07-18 01:36:59 +0200

    chore: final report with timestamp delay implemented

--- test/oracle/test-each-single-source.test.ts
+++ test/oracle/test-each-single-source.test.ts
@@ -1,5 +1,8 @@
 import redstone from "../../src/index";
-import { DataFeedId, DataSourcesConfig } from "../../src/oracle/redstone-data-feed";
+import {
+  DataFeedId,
+  DataSourcesConfig,
+} from "../../src/oracle/redstone-data-feed";
 
 const MAX_TIME_DIFF = 150000; // 150s
 
@@ -12,29 +15,39 @@ const dataFeedIds = [
   "redstone-custom-urls-demo",
 ] as DataFeedId[];
 
-describe("Test all sources separately", () => {
+const finalReport: { [dataSourceId: string]: string } = {};
 
+describe("Test all sources separately", () => {
   for (const dataFeedId of dataFeedIds) {
     describe(`Test data sources for: ${dataFeedId}`, () => {
-      const defaultSourcesConfig = redstone.oracle.getDefaultDataSourcesConfig(dataFeedId);
+      const defaultSourcesConfig = redstone.oracle.getDefaultDataSourcesConfig(
+        dataFeedId
+      );
       const sourcesCount = defaultSourcesConfig.sources.length;
-  
+
       // Testing each data source seprately
       for (let sourceIndex = 0; sourceIndex < sourcesCount; sourceIndex++) {
         const source = defaultSourcesConfig.sources[sourceIndex];
 
         const shortUrl = source.url || "";
-        const sourceDescription = `${dataFeedId} ${sourceIndex + 1}/${sourcesCount} (${source.type}: ${shortUrl})`;
+        const sourceDescription = `${dataFeedId} ${
+          sourceIndex + 1
+        }/${sourcesCount} (${source.type}: ${shortUrl})`;
         test(`Should fetch using source: ${sourceDescription})`, async () => {
           await testFetching(sourceDescription, {
             ...defaultSourcesConfig,
-            sources: [ source ], // Single source
+            maxTimestampDiffMilliseconds: 365 * 24 * 3600 * 1000,
+            sources: [source], // Single source
           });
         });
       }
+
+      afterAll(() => {
+        console.log("=== FINAL REPORT ===");
+        console.log(finalReport);
+      });
     });
   }
-
 });
 
 async function testFetching(
@@ -44,7 +57,8 @@ async function testFetching(
 ) {
   const dataPackage = await redstone.oracle.get(dataSourcesConfig, asset);
   const timestampDiff = Date.now() - dataPackage.priceData.timestamp;
-  expect(timestampDiff).toBeLessThan(MAX_TIME_DIFF);
   const secondsDiff = timestampDiff / 1000;
+  finalReport[sourceDescription] = secondsDiff + "s";
+  expect(timestampDiff).toBeLessThan(MAX_TIME_DIFF);
   console.log(`${sourceDescription}: ${secondsDiff}s delay`);
 }
